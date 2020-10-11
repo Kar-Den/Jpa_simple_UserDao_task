99cdc74887bdf60aec8cc70b780b415d8de52dc0
Kar-Den <k.denis.vlad@gmail.com>
2020-10-11 08:04:52 +0300

    UtilClass for EntityManagerFactory()

---
+++ src/main/java/by/it/JpaEntityManagerFactoryUtil.java
@@ -0,0 +1,25 @@
+package by.it;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JpaEntityManagerFactoryUtil {
+
+    private static final EntityManagerFactory ENTITY_MANAGER_FACTORY;
+
+    static {
+        ENTITY_MANAGER_FACTORY = Persistence.createEntityManagerFactory("test");
+    }
+
+    private JpaEntityManagerFactoryUtil(){
+    }
+
+    public static EntityManagerFactory getEntityManagerFactory(){
+        return ENTITY_MANAGER_FACTORY;
+    }
+
+    public static EntityManager getEntityManager() {
+        return ENTITY_MANAGER_FACTORY.createEntityManager();
+    }
+}
