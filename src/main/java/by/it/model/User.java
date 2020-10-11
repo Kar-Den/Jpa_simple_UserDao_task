c2788966abf2bdc6123b7fcab762397e4438e572
Kar-Den <k.denis.vlad@gmail.com>
2020-10-11 08:06:00 +0300

    сделал класс User

---
+++ src/main/java/by/it/model/User.java
@@ -0,0 +1,45 @@
+package by.it.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@AllArgsConstructor
+@Builder
+@Table (name = "users")
+public class User {
+    @Id
+    @GeneratedValue
+    private int id;
+    @Column (name = "first_name")
+    private String firstName;
+    @Column (name = "last_name")
+    private String lastName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
